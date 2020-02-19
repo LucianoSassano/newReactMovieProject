317d273fad2fc2727d7ae9dc5a971260b0541214
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-19 18:27:06 -0300

    styling the form and adding responsivness

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,10 @@ class Navbar extends React.Component {
             >
               <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
               <ul className="navbar-nav mr-auto">
                 <li className="nav-item active">
                   <a className="nav-link" href="./Home">
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,64 @@
-import React from 'react'
-import NavBar from '../components/NavBar'
+import React from "react";
+import NavBar from "../components/NavBar";
+import "../pages/Login.css";
 
-class Login extends React.Component
-{
-    render(){
-        return(
-            <>
+class Login extends React.Component {
+  render() {
+    return (
+      <>
         <NavBar />
-        <form>
-        <div class="row">
-    <div class="col">
-      <input type="text" class="form-control" placeholder="First name"/>
-    </div>
-    <div class="col">
-      <input type="text" class="form-control" placeholder="Last name"/>
-    </div>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputEmail1">Email</label>
-    <input type="text" class="form-control" placeholder="Email"/>
-  </div>
-  <div className="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="text" class="form-control" placeholder="Password"/>
-  </div>
-  <button type="submit" className="btn btn-primary">Ingresar</button>
-</form>
-    </>
-    )}
+        <form className="form-class">
+          <div className="col-auto">
+          <div class="col-auto">
+            <div class="name-input">
+              <label>name</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="First name"
+                required
+              />
+            </div>
+            <div class="last-name-input">
+              <label>last name</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Last name"
+                required
+              />
+            </div>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Email</label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="email"
+            />
+            <small id="emailHelp" class="form-text text-muted">
+              We'll never share your email with anyone else.
+            </small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              class="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Ingresar
+          </button>
+          </div>
+        </form>
+      </>
+    );
+  }
 }
 
-
-export default Login
+export default Login;

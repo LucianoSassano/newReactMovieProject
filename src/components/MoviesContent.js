e99f77ec63b3de74988c3eea54413de75de9bc79
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-20 18:38:50 -0300

    progress

--- src/components/MoviesContent.js
+++ src/components/MoviesContent.js
@@ -1,61 +1,67 @@
 import React, { Fragment } from "react";
 
-
 export default class MovieContent extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       page: [],
       results: []
-      
     };
-    
   }
 
-  componentDidMount(){
-    this.performSearch("woman")
+  componentDidMount() {
+    this.performSearch("batman");
   }
-  
 
-  performSearch = async (searchTerm) => {
+  performSearch = async searchTerm => {
     console.log("Performing search");
     const urlString =
       "https://api.themoviedb.org/3/search/movie?api_key=16e4c7896761f5945c9296a93ce5703f&query=" +
       searchTerm;
 
-      try{
-        const response = await fetch(urlString)
-        const resJson = response.json()
-        const {results} = await resJson
-        
+    try {
+      const response = await fetch(urlString);
+      const resJson = response.json();
+      const { results } = await resJson;
 
-        this.setState({
-          results,
-        })
-      } catch(err) {
-        console.error('fail')
-      }
+      this.setState({
+        results
+      });
+    } catch (err) {
+      console.error("fail");
+    }
+  };
+
+  handleSearchValue() {
+    const searchTerm = this.props.search;
+    this.performSearch(searchTerm);
   }
 
   render() {
     const { results } = this.state;
     return (
       <div className="container">
-        {results && results.map((movie, key) => (
-          <Fragment key={key}>
-            <img
-              src={
-                "https://image.tmdb.org/t/p/w200" + movie.poster_path}
-            ></img>
-            <h1>{movie.title}</h1>
-            <div>
-              <p>{movie.overview}</p>
+        {results &&
+          results.map((movie, key) => (
+            <div className="row">
+              <Fragment key={key}>
+                <div className="col">
+                  <img
+                    src={"https://image.tmdb.org/t/p/w200" + movie.poster_path}
+                  ></img>
+                </div>
+                <div className="col">
+                  <h1>{movie.title}</h1>
+                  <div>
+                    <p>{movie.overview}</p>
+                  </div>
+                  <div>
+                    <p>Release date : {movie.release_date}</p>
+                  </div>
+                </div>
+              </Fragment>
             </div>
-            <div>
-              <p>Release date : {movie.release_date}</p>
-            </div>
-          </Fragment>
-        ))}
+          ))}
       </div>
     );
   }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,6 @@ class Home extends React.Component {
 
   handleSearch(value){
     console.log(value);
-    
     this.setState({search : value});
   }
 
@@ -24,7 +23,7 @@ class Home extends React.Component {
           onSearch={(e)=> this.handleSearch(e.target.value) }
           search={this.state.search}
         />
-        <MovieContent queryValue={this.state.search}/>
+        <MovieContent search={this.state.search}/>
       </>
     );
   }

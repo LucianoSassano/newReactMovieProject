5920d2ff1b2ec48bea38784d202c07680c504dec
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-20 11:08:23 -0300

    working

--- src/components/MoviesContent.js
+++ src/components/MoviesContent.js
@@ -1,50 +1,81 @@
-import React from "react";
+import React, { Fragment } from "react";
 import $ from "jquery";
-import Movie from "../components/Movie";
 
 export default class MovieContent extends React.Component {
   constructor(props) {
     super(props);
-  
     this.state = {
       page: [],
       results: []
     };
-    this.performSearch();
+  }
+  
+  componentDidMount() {
+    this.performSearch("avengers");
   }
 
-  performSearch(searchTerm) {
+  // performSearch(searchTerm) {
+  //   console.log("Performing search");
+  //   const urlString =
+  //     "https://api.themoviedb.org/3/search/movie?api_key=16e4c7896761f5945c9296a93ce5703f&query=" +
+  //     searchTerm;
+  //   $.ajax({
+  //     url: urlString,
+  //     success: searchResults => {
+  //       console.log("performed succesfully");
+  //       const results = searchResults.results;
+  //       console.log(results);
+        
+  //       this.setState({
+  //         results: results,
+  //       })
+  //     },
+  //     error: (xhr, status, err) => {
+  //       console.error("failed to fetch data");
+  //     }
+  //   });
+  // }
+
+  performSearch = async (searchTerm) => {
     console.log("Performing search");
     const urlString =
       "https://api.themoviedb.org/3/search/movie?api_key=16e4c7896761f5945c9296a93ce5703f&query=" +
       searchTerm;
-    $.ajax({
-      url: urlString,
-      success: searchResults => {
-        console.log("performed succesfully");
-        const results = searchResults.results;
 
-        results.forEach(movie => {
-          console.log(movie.title);
-        });
-      },
-      error: (xhr, status, err) => {
-        console.error("failed to fetch data");
+      try{
+        const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=16e4c7896761f5945c9296a93ce5703f&query=avengers`)
+        const resJson = response.json()
+        const {results} = await resJson
+        
+
+        this.setState({
+          results,
+        })
+      } catch(err) {
+        console.error('fail')
       }
-    });
   }
 
   render() {
+    const { results } = this.state;
     return (
       <div className="container">
-        {this.state.results.map((movie, key) => (
-          <Movie
-            img={"https://image.tmdb.org/t/p/w185" + movie.poster_path}
-            title={movie.title}
-            overview={movie.overview}
-            release={movie.release_date}
-            key={key}
-          />
+        {results && results.map((movie, key) => (
+          <Fragment key={key}>
+          {console.log('ddasadsads', movie.title)
+          }
+            <img
+              src={
+                "https://image.tmdb.org/t/p/w500" + movie.poster_path}
+            ></img>
+            <h1>{movie.title}</h1>
+            <div>
+              <p>{movie.overview}</p>
+            </div>
+            <div>
+              <p>Release date : {movie.release_date}</p>
+            </div>
+          </Fragment>
         ))}
       </div>
     );

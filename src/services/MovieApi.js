2605abc6df74036122dda97ce990655ba9649aac
Luciano_Sassano <lucianosassano1@gmail.com>
2020-02-20 23:26:24 -0300

    services file

---
+++ src/services/MovieApi.js
@@ -0,0 +1,70 @@
+async function getBySearch(searchTerm) {
+  console.log("Performing search");
+
+  const urlString =
+    "https://api.themoviedb.org/3/search/movie?api_key=16e4c7896761f5945c9296a93ce5703f&query=" +
+    searchTerm;
+
+  try {
+    const response = await fetch(urlString);
+    const resJson = response.json();
+    const { results } = await resJson;
+
+    return results;
+  } catch (err) {
+    console.error("fail");
+  }
+}
+
+async function getLatest() {
+  console.log("Performing search");
+
+  const urlString =
+    "https://api.themoviedb.org/3/search/movie/latest?api_key=16e4c7896761f5945c9296a93ce5703f&language=en-US";
+
+  try {
+    const response = await fetch(urlString);
+    const resJson = response.json();
+    const { results } = await resJson;
+
+    return results;
+  } catch (err) {
+    console.error("fail");
+  }
+}
+
+async function getPopular() {
+  console.log("Performing search");
+
+  const urlString =
+    "https://api.themoviedb.org/3/search/movie/popular?api_key=16e4c7896761f5945c9296a93ce5703f&language=en-US";
+
+  try {
+    const response = await fetch(urlString);
+    const resJson = response.json();
+    const { results } = await resJson;
+
+    return results;
+  } catch (err) {
+    console.error("fail");
+  }
+}
+
+async function getTopRated() {
+    console.log("Performing search");
+  
+    const urlString =
+      "https://api.themoviedb.org/3/search/movie/top_rated?api_key=16e4c7896761f5945c9296a93ce5703f&language=en-US";
+  
+    try {
+      const response = await fetch(urlString);
+      const resJson = response.json();
+      const { results } = await resJson;
+  
+      return results;
+    } catch (err) {
+      console.error("fail");
+    }
+  }
+
+export { getBySearch, getLatest, getPopular ,getTopRated};
